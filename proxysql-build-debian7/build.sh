224f7301ec392f3de59a2bbd13a2b6073c7a648b
René Cannaò <rene.cannao@gmail.com>
2015-12-23 03:00:08 +0000

    Add support for debian 7.8 and 8.2

---
+++ proxysql-build-debian7/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build -t renecannao/proxysql:build-debian7 proxysql-build-debian7
---
+++ proxysql-build-debian8/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build -t renecannao/proxysql:build-debian8 proxysql-build-debian8
