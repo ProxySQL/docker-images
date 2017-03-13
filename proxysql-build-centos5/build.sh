99d7fbb238899f1742886b09e54c7aa880ba281d
René Cannaò <rene.cannao@gmail.com>
2017-03-13 11:25:37 +0000

    Added support for Fedora 24 and CentOS 5

---
+++ proxysql-build-centos5/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build -t renecannao/proxysql:build-centos5 proxysql-build-centos5
---
+++ proxysql-build-fedora24/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build -t renecannao/proxysql:build-fedora24 proxysql-build-fedora24
